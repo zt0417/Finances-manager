01492a3278de84c5f157c39088f63f3aab34a83e
DESKTOP-VUORLUU\zt0417 <zt04170@gmail.com>
2017-03-23 08:38:56 +0800

    FragmentTabHost

---
+++ Financesmanager/app/src/main/java/com/tongzhang/financesmanager/fragment/HomeFragment.java
@@ -0,0 +1,27 @@
+package com.tongzhang.financesmanager.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tongzhang.financesmanager.R;
+
+/**
+ * Created by zt041 on 2017-03-22.
+ */
+
+public class HomeFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+
+    //@Override
+    public void init() {
+
+    }
+}
