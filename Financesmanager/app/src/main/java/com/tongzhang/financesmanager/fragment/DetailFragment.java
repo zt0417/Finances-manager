b9dbb2c7a2d5134cf528d0457df511a6abf1eb76
DESKTOP-VUORLUU\zt0417 <zt04170@gmail.com>
2017-06-01 16:10:33 +0800

    060101

--- Financesmanager/app/src/main/java/com/tongzhang/financesmanager/fragment/DetailFragment.java
+++ Financesmanager/app/src/main/java/com/tongzhang/financesmanager/fragment/DetailFragment.java
@@ -6,14 +6,20 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.tongzhang.financesmanager.R;
+
 
 /**
  * Created by zt041 on 2017-03-22.
  */
-
 public class DetailFragment extends Fragment{
+
+
+    private TextView mTv;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,5 +30,31 @@ public class DetailFragment extends Fragment{
     public void init() {
 
     }
-
 }
+
+/*public class DetailFragment extends Fragment{
+
+    private TextView mTv;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        final View view=inflater.inflate(R.layout.fragment_detail,container,false);
+        mTv = (TextView) view.findViewById(R.id.bt_main);
+        helloEventBus();
+        return view;
+    }
+
+    @Subscribe(threadMode = ThreadMode.MainThread)
+    public void helloEventBus(String message){
+        mText.setText(message);
+    }
+
+
+    //@Override
+    public void init() {
+
+    }
+
+}*/
